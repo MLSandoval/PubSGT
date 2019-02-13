7aa31370031d06380de343023d773b9ac886c6aa
Scott Bowler <scott.bowler@learningfuze.com>
2019-02-13 12:22:16 -0800

    Fixed typo in header config

--- server/startup.php
+++ server/startup.php
@@ -3,7 +3,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Request-Method: POST");
-header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 
 require_once('functions.php');
 ?>
